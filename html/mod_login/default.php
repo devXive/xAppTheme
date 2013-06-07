68cfed3b56552162edb6f547835f8bfd011bca7d
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-06-07 10:47:50 +0200

    added icons fix password reset link, fix login/create module text

--- html/mod_login/default.php
+++ html/mod_login/default.php
@@ -11,14 +11,20 @@ defined('_JEXEC') or die;
 
 JHtml::_('behavior.keepalive');
 JHtml::_('bootstrap.tooltip');
+
+$usersConfig = JComponentHelper::getParams('com_users');
 ?>
 <a class="user-menu dropdown-toggle" href="#" data-toggle="dropdown">
-	<i class="icon-signin"> <?php echo JText::_('JLOGIN') . '/' . JText::_('MOD_LOGIN_REGISTER'); ?> </i>
+	<? if ($usersConfig->get('allowUserRegistration')) : ?>
+		<i class="icon-signin"> <?php echo JText::_('JLOGIN') . '/' . JText::_('MOD_LOGIN_REGISTER'); ?> </i>
+	<? else: ?>
+		<i class="icon-signin"> <?php echo JText::_('JLOGIN'); ?> </i>
+	<? endif; ?>
 	<i class="icon-caret-down"></i>
 </a>
-<ul id="user_menu" class="pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-closer" style="padding-top: 0;">
+<ul id="user_menu" class="pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-closer" style="padding: 0;">
 	<li>
-		<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" class="well" style="margin-bottom: 5px;">
+		<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" class="well" style="margin-bottom: 0;">
 			<?php if ($params->get('pretext')) : ?>
 				<p><?php echo $params->get('pretext'); ?></p>
 			<?php endif; ?>
@@ -46,18 +52,18 @@ JHtml::_('bootstrap.tooltip');
 		</form>
 	</li>
 	<?php
-		$usersConfig = JComponentHelper::getParams('com_users');
 		if ($usersConfig->get('allowUserRegistration')) : ?>
 			<li>
 				<a href="<?php echo JRoute::_('index.php?option=com_users&view=registration'); ?>">
-				<?php echo JText::_('MOD_LOGIN_REGISTER'); ?> <span class="icon-arrow-right"></span></a>
+				<span style="width: 20px; display: inline-block;"><i class="icon-asterisk"></i></span> <?php echo JText::_('MOD_LOGIN_REGISTER'); ?></a>
 			</li>
 			<li>
 				<a href="<?php echo JRoute::_('index.php?option=com_users&view=remind'); ?>">
-				  <?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></a>
-			</li>
-			<li>
-				<a href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>"><?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
+				<span style="width: 20px; display: inline-block;"><i class="icon-user"></i></span> <?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></a>
 			</li>
 	<?php endif; ?>
+			<li>
+				<a href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>">
+				<span style="width: 20px; display: inline-block;"><i class="icon-lock"></i></span> <?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
+			</li>
 </ul>
